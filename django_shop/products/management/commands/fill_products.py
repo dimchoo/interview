d24dcbe8d0060efa21c48a4b017cf5991875997e
dimchoo <imdimchoo@gmail.com>
2022-06-02 17:06:55 +0300

    lesson5

--- django_shop/products/management/commands/fill_products.py
+++ django_shop/products/management/commands/fill_products.py
@@ -1,12 +1,18 @@
 from django.core.management.base import BaseCommand
 import random
-from products.models import Supplier, Product
+from products.models import Supplier, Category, Product
 
 
 SUPPLIER_TEST_DATA = [
     {'name': 'Coca-Cola Co'},
     {'name': 'Procter & Gamble'},
     {'name': 'Apple'},
+]
+
+CATEGORIES_TEST_DATA = [
+    {'name': 'Напитки'},
+    {'name': 'Техника'},
+    {'name': 'Кондитерские изделия'},
 ]
 
 
@@ -27,17 +33,41 @@ PRODUCT_TEST_DATA = [
         'name': 'Сок',
         'price': round(random.random() * 10000, 2),
     },
+    {
+        'name': 'Курица',
+        'price': round(random.random() * 10000, 2),
+    },
+    {
+        'name': 'Халва',
+        'price': round(random.random() * 10000, 2),
+    },
+    {
+        'name': 'Coca-Cola',
+        'price': round(random.random() * 10000, 2),
+    },
+    {
+        'name': 'Kinder',
+        'price': round(random.random() * 10000, 2),
+    },
+
 ]
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         Supplier.objects.all().delete()
+        Category.objects.all().delete()
         Product.objects.all().delete()
 
         Supplier.objects.bulk_create([Supplier(**i) for i in SUPPLIER_TEST_DATA])
-        ids = list(Supplier.objects.all().values_list('id', flat=True))
+        sup_ids = list(Supplier.objects.all().values_list('id', flat=True))
         print('Suppliers done!')
 
-        Product.objects.bulk_create([Product(**i, supplier_id=random.choice(ids)) for i in PRODUCT_TEST_DATA])
+        Category.objects.bulk_create([Category(**i) for i in CATEGORIES_TEST_DATA])
+        cat_ids = list(Supplier.objects.all().values_list('id', flat=True))
+        print('Categories done!')
+
+        Product.objects.bulk_create([Product(**i, supplier_id=random.choice(sup_ids)) for i in PRODUCT_TEST_DATA])
+        for product in Product.objects.all().iterator():
+            product.categories.set(list(set([random.choice(cat_ids), random.choice(cat_ids)])))
         print('Products done!')
